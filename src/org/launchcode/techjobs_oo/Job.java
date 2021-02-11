de0223cb61a0deb658048b9c045542cf865ea790
karaunderwood <karaunderwooddesign@gmail.com>
2021-02-11 13:52:40 -0600

    finished dry step

--- src/org/launchcode/techjobs_oo/Job.java
+++ src/org/launchcode/techjobs_oo/Job.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 public class Job {
 
+    // Variables/fields/attributes/whatever the heck these are called
     private int id;
     private static int nextId = 1;
 
@@ -13,7 +14,7 @@ public class Job {
     private PositionType positionType;
     private CoreCompetency coreCompetency;
 
-    // TODO: Add two constructors - one to initialize a unique ID and a second to initialize the
+    //  Add two constructors - one to initialize a unique ID and a second to initialize the
     //  other five fields. The second constructor should also call the first in order to initialize
     //  the 'id' field.
 
@@ -31,8 +32,7 @@ public class Job {
         this.coreCompetency = coreCompetency;
     }
 
-    // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
-    //  match.
+    // Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields match.
 
     @Override
     public boolean equals(Object o) {
@@ -47,6 +47,32 @@ public class Job {
         return Objects.hash(getId());
     }
 
+    @Override
+    public String toString() {
+        if (this.name == ""){
+            this.name = "Data not available";
+        }
+        if (this.employer.getValue() == ""){
+            this.employer.setValue("Data not available");
+        }
+        if (this.location.getValue() == ""){
+            this.location.setValue("Data not available");
+        }
+        if (this.positionType.getValue() == ""){
+            this.positionType.setValue("Data not available");
+        }
+        if (this.coreCompetency.getValue() == ""){
+            this.coreCompetency.setValue("Data not available");
+        }
+        return
+                "\nID: " + id +
+                "\nName: " + name +
+                "\nEmployer: " + employer +
+                "\nLocation: " + location +
+                "\nPosition Type: " + positionType +
+                "\nCore Competency: " + coreCompetency +
+                "\n";
+    }
 
     // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
     //  and id.
--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -15,6 +15,7 @@ public class JobTest {
     Job test_Job2;
     Job test_Job3;
     Job test_Job4;
+    Job test_Job5;
     @Before
     public void createFirstJobObject() {
         test_Job1 = new Job();
@@ -37,11 +38,17 @@ public class JobTest {
                 new PositionType("Quality control"), new CoreCompetency("Persistence"));
     }
 
-    //TODO: test that the ID values for the two objects are NOT the same and differ by 1
+    @Before
+    public void createFifthJobObject() {
+        test_Job5 = new Job("", new Employer(""), new Location(""),
+                new PositionType(""), new CoreCompetency(""));
+    }
+
+
+    // Test that the ID values for the two objects are NOT the same and differ by 1
     @Test
     public void testSettingJobId() {
-        assertEquals(1, test_Job1.getId(), .001);
-        assertEquals(2, test_Job2.getId(), .001);
+        assertEquals(1, test_Job2.getId() - test_Job1.getId());
     }
 
     // Test the full constructor
@@ -57,7 +64,39 @@ public class JobTest {
     @Test
     public void testJobsForEquality() {
         assertFalse(test_Job3.equals(test_Job4));
+        assertTrue(test_Job4.equals(test_Job4));
     }
 
+
+    // When passed a Job object, it should return a string that contains a blank line before and after the job information.
+    @Test
+    public void testJobsToReturnStringSurroundedByBlankLines(){
+        int length = test_Job3.toString().length();
+
+        assertSame(test_Job3.toString().charAt(0), '\n');
+        assertSame(test_Job3.toString().charAt(length - 1), '\n');
+    }
+
+
+    // The string should contain a label for each field, followed by the data stored in that field. Each field should be on its own line.
+    @Test
+    public void TestJobsToStringToContainLabelsDataAndNewLines() {
+        assertEquals(test_Job3.toString(),
+                "\nID: " + test_Job3.getId() +
+                        "\nName: " + test_Job3.getName() +
+                        "\nEmployer: " + test_Job3.getEmployer() +
+                        "\nLocation: " + test_Job3.getLocation() +
+                        "\nPosition Type: " + test_Job3.getPositionType() +
+                        "\nCore Competency: " + test_Job3.getCoreCompetency() +
+                        "\n"
+                );
+    }
+
+
+    // If a field is empty, the method should add, “Data not available” after the label.
+    @Test
+    public void testJobsWithNoData() {
+        assertTrue(test_Job5.toString().contains("Data not available"));
+    }
 
 }
